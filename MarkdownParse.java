38fc150e8955aabe413a0f124b7ce862a6172d29
stanley-pan <s5pan@ucsd.edu>
2022-01-26 17:49:14 -0800

    implemented hamcrest and junit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -27,5 +27,6 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
+        System.out.println("hello");
     }
 }
