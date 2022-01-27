5dc22bc621e1efa35c19c7945762855a1d2dddfc
stanley-pan <s5pan@ucsd.edu>
2022-01-26 18:32:33 -0800

    added markdownparse test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,6 +18,10 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(currentIndex < 0){
+                System.out.println("Code broke!");
+                break;
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
