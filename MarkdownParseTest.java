2176c0caaf57d26d1763445b784f3313649a2955
stanley-pan <s5pan@ucsd.edu>
2022-01-27 19:20:09 -0800

    added commands to compile and run JUnit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,3 +20,9 @@ public class MarkdownParseTest {
         assertEquals(linkList, links);
     }
 }
+
+/**
+ * To compile the tests:
+ * javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+ * java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+ */
