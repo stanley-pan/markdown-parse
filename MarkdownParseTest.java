aad0d8aedd5ce0ba6105176fbec1046a1225e6b5
stanley-pan <s5pan@ucsd.edu>
2022-02-02 18:26:07 -0800

    added wrong test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,7 +9,7 @@ import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 1);
     }
     @Test
     public void getLinks() throws IOException{
