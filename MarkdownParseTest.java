8c975d492ebf562f681019ddccc6b17aa21015be
stanley-pan <s5pan@ucsd.edu>
2022-02-02 17:59:56 -0800

    Added more testing/expected nul

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,12 +12,36 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
     @Test
-    public void getLinks(){
+    public void getLinks() throws IOException{
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> linkList = List.of("https://something.com", "some-page.html");
-        assertEquals(linkList, links);
+        assertEquals(null, links);
+    }
+    @Test
+    public void getLinks2() throws IOException{
+        Path fileName = Path.of("test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        List<String> linkList = List.of();
+        assertEquals(null, links);
+    }
+    @Test
+    public void getLinks3() throws IOException{
+        Path fileName = Path.of("test-file2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        List<String> linkList = List.of("https://som(eth)ing.com");
+        assertEquals(null, links);
+    }
+    @Test
+    public void getLinks4() throws IOException{
+        Path fileName = Path.of("test-file4.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        List<String> linkList = List.of();
+        assertEquals(null, links);
     }
 }
 
