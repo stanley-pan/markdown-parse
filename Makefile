c5a99466f20eae21bd4fc36e9799918468395ab1
stanley-pan <s5pan@ucsd.edu>
2022-02-09 18:37:43 -0800

    created Makefile and mdparse

---
+++ Makefile
@@ -0,0 +1,3 @@
+test: MarkdownParse.java
+	javac MarkdownParse.java
+
--- MarkdownParse.java
+++ MarkdownParse.java
@@ -42,6 +42,5 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
-        System.out.println("hello");
     }
 }
